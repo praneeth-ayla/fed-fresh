9d47b4e2f010e21d3e97d971f89884fa4b56c66b
ran <praneethayla@gmail.com>
2025-11-08 16:26:24 +0530

    navbar & color defaults

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,60 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const path = usePathname();
+
+  const isSelectedPath = (currentPath: string, expectedPath: string) =>
+    currentPath === expectedPath || currentPath.startsWith(expectedPath + "/");
+
   return (
-    <nav className="flex justify-between px-16 py-5 border-b-2">
+    <nav className="bg-[#FFFDF6] px-16 flex items-center justify-between h-24 text-2xl">
       <div>Logo</div>
-      <div className="flex gap-8">
-        <div className="flex gap-4">
-          <Link href="/">Home</Link>
-          <Link href="/menu">Menu</Link>
-          <Link href="/orders">Track your order</Link>
-          <Link href="/cart">Cart</Link>
-        </div>
+      <div className="flex gap-12">
+        <Link
+          href="/"
+          className={`${
+            isSelectedPath(path, "/")
+              ? "font-semibold border-b-4 border-surface"
+              : ""
+          }`}
+        >
+          Home
+        </Link>
+
+        <Link
+          href="/menu"
+          className={`${
+            isSelectedPath(path, "/menu")
+              ? "font-semibold border-b-4 border-surface"
+              : ""
+          }`}
+        >
+          Menu
+        </Link>
+
+        <Link
+          href="/orders"
+          className={`${
+            isSelectedPath(path, "/orders")
+              ? "font-semibold border-b-4 border-surface"
+              : ""
+          }`}
+        >
+          Track your order
+        </Link>
+
+        <Link
+          href="/cart"
+          className={`${
+            isSelectedPath(path, "/cart")
+              ? "font-semibold border-b-4 border-surface"
+              : ""
+          }`}
+        >
+          Cart
+        </Link>
       </div>
     </nav>
   );
